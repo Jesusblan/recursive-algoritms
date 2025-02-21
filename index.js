045183bcf22f6ade49aaba76e06e2570ecc249b3
JesusBlan <jezublanco@gmail.com>
2025-02-21 12:35:40 +0100

    finally i know how to calculate a fibonacci number recursively and how to store it in an array, time complexity was hard to chew

--- index.js
+++ index.js
@@ -4,16 +4,18 @@
 //something like this: 1:1, 2:1, 3:2, 4:3, 5:5, 6:8 ...
 //fib(n) = fib(n-1) + fib(n-2)
 //if n < 2, n is the fibonacci number, return it
+//finally working and storing all numbers in an array!
 
-
+arr = [0, 1]
 
 function fib(n){
     if (n < 2){
         return n;
     }
 
-    return fib(n - 1) + fib(n-2);
+    return arr[n] = fib(n - 1) + fib(n - 2);
 };
 
-console.log(fib(9))
+console.log(fib(40))
+console.log(arr)
 
